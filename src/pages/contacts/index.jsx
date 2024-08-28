19a25536f110aeb3b07d511a5ca6b9d353cee343
yanrodrigues205 <yan.pablo205@gmail.com>
2024-08-28 17:35:43 -0300

    :sparkles: feat: cards with contact information

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -1,9 +1,72 @@
-import { Container } from "../../styles/contacts/style";
+import { Container, SocialMedia, SocialMediaDescription, SocialMediaIcon, SocialMediaItems, SocialMediaLink, SocialMediaTitle } from "../../styles/contacts/style";
+import {FaEnvelope, FaLinkedin, FaGithub, FaPhone, FaArrowRight} from "react-icons/fa";
 export default function Contacts()
 {
     return (
         <>
-            <Container><h1>contacts</h1></Container>
+            <Container>
+                <SocialMedia>
+                    <SocialMediaItems>
+                        <SocialMediaIcon>
+                            <FaEnvelope/>
+                        </SocialMediaIcon>
+                        <SocialMediaTitle>
+                            E-mail
+                        </SocialMediaTitle>
+                        <SocialMediaDescription>
+                            E-mail address is a good option to contact me about hiring and formal matters.
+                        </SocialMediaDescription>
+                        <SocialMediaLink>
+                            Go to <FaArrowRight/>
+                        </SocialMediaLink>
+                    </SocialMediaItems>
+
+                    <SocialMediaItems>
+                        <SocialMediaIcon>
+                            <FaPhone/>
+                        </SocialMediaIcon>
+                        <SocialMediaTitle>
+                            Phone
+                        </SocialMediaTitle>
+                        <SocialMediaDescription>
+                        Used to deal with extremely critical issues or situations of a personal nature.
+                        </SocialMediaDescription>
+                        <SocialMediaLink>
+                            Go to <FaArrowRight/>
+                        </SocialMediaLink>
+                    </SocialMediaItems>
+
+                    <SocialMediaItems>
+                        <SocialMediaIcon>
+                            <FaLinkedin/>
+                        </SocialMediaIcon>
+                        <SocialMediaTitle>
+                            LinkedIn
+                        </SocialMediaTitle>
+                        <SocialMediaDescription>
+                        Social network that I am using most of the time, great for following my professional career, with the respective companies and positions.
+                        </SocialMediaDescription>
+                        <SocialMediaLink>
+                            Go to <FaArrowRight/>
+                        </SocialMediaLink>
+                    </SocialMediaItems>
+
+                    <SocialMediaItems>
+                        <SocialMediaIcon>
+                            <FaGithub/>
+                        </SocialMediaIcon>
+                        <SocialMediaTitle>
+                            GitHub
+                        </SocialMediaTitle>
+                        <SocialMediaDescription>
+                            A great platform to follow my development activities in public projects, with several options for generating metrics.
+                        </SocialMediaDescription>
+                        <SocialMediaLink>
+                            Go to <FaArrowRight/>
+                        </SocialMediaLink>
+                    </SocialMediaItems>
+                </SocialMedia>
+            </Container>
         </>
     )
 }
--- src/styles/contacts/style.js
+++ src/styles/contacts/style.js
@@ -2,5 +2,108 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
-    max-width: 1190px;
+    max-width: 1120px;
+    flex-direction: column;
+`;
+
+export const SocialMedia = styled.div`
+    margin-top: 5%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    max-width: 1120px;
+    width: 100%;
+    height: 100%;
+    padding: 15px;
+    gap: 20px;
+
+    @media(max-width: 1160px)
+    {
+        grid-template-columns: repeat(2, 1fr);
+        margin: 5% auto;
+        padding: 3%;
+        
+    }
+
+    @media(max-width: 850px)
+    {
+        padding: 5%;
+        grid-template-columns: repeat(1, 1fr);
+    }
+
+`;
+
+export const SocialMediaItems = styled.div`
+    display: flex;
+    width:100%;
+    flex-direction: column;
+    color: black;
+    height: 340px;
+    padding: 10px;
+    border-radius: 20px;
+    box-shadow: 1px 1px 18px rgba(2, 172, 202, 0.6);
+    &:hover
+    {
+        box-shadow: 1px 1px 18px rgba(2, 172, 202, 1);
+    }
+
+`; 
+
+export const SocialMediaIcon = styled.div`
+    display: flex;
+    width: 90px;
+    height: 90px;
+    background-color: #02ACCA;
+    color: black;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 45px;
+    margin: 0 auto;
+    margin-top: 20px;
+    border-radius: 110px;
 `
+
+export const SocialMediaTitle = styled.span`
+    display: flex;
+    width: 100%;
+    text-align: center;
+    height: 20%;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    color: white;
+`;
+
+export const SocialMediaDescription =  styled.span`
+    width: 100%;
+    height: 20%;
+    text-align: start;
+    color: white;
+    font-size: 15px;
+    font-family: "Nunito", sans-serif;
+`;
+
+export const SocialMediaLink = styled.span`
+    display: flex;
+    width: 60%;
+    margin: 0 auto;
+    height: 15%;
+    margin-top: 7%;
+    background: #02ACCA;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    border-radius: 13px;
+    color: black;
+    cursor: pointer;
+
+    &:hover
+    {
+        box-shadow: 1px 1px 18px rgba(2, 172, 202, 1);
+    }
+    svg
+    {
+        font-size: 20px;
+    }
+`;
